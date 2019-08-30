456378bd4f42243766b0270ba33133521e8a6da8
Remco Verhoef <remco@dutchcoders.io>
2019-08-30 16:02:19 +0200

    working on tests

--- consume_test.go
+++ consume_test.go
@@ -3,105 +3,11 @@ package ravenworker
 import (
 	"bytes"
 	"fmt"
-	"net"
-	"os"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	uuid "github.com/satori/go.uuid"
-	"zombiezen.com/go/capnproto2/rpc"
 )
-
-var (
-	flowID   = uuid.NewV4()
-	workerID = uuid.NewV4()
-)
-
-// important to know is that you
-// cannot return t.Fatalf / t.FailNow()
-// from within go routines
-func TestMain(m *testing.M) {
-
-	// call flag.Parse() here if TestMain uses flags
-	os.Exit(m.Run())
-}
-
-type workflowServer struct {
-	getEvent       func(getEvent Workflow_getEvent) error
-	getJob         func(getJob Workflow_getJob) error
-	ackJob         func(ackJob Workflow_ackJob) error
-	putEvent       func(putEvent Workflow_putEvent) error
-	getLatestEvent func(getLatestEvent Workflow_getLatestEvent) error
-}
-
-func (w *workflowServer) PutEvent(putEvent Workflow_putEvent) error {
-	if w.putEvent != nil {
-		return w.putEvent(putEvent)
-	}
-
-	return fmt.Errorf("putEvent not configured")
-}
-
-func (w *workflowServer) AckJob(ackJob Workflow_ackJob) error {
-	if w.ackJob != nil {
-		return w.ackJob(ackJob)
-	}
-
-	return fmt.Errorf("ackJob not configured")
-}
-
-func (w *workflowServer) GetJob(getJob Workflow_getJob) error {
-	if w.getJob != nil {
-		return w.getJob(getJob)
-	}
-
-	return fmt.Errorf("getJob not configured")
-}
-
-func (w *workflowServer) GetLatestEvent(getLatestEvent Workflow_getLatestEvent) error {
-	if w.getLatestEvent != nil {
-		return w.getLatestEvent(getLatestEvent)
-	}
-
-	return fmt.Errorf("getLatestEvent not configured")
-}
-
-func (w *workflowServer) GetEvent(getEvent Workflow_getEvent) error {
-	if w.getEvent != nil {
-		return w.getEvent(getEvent)
-	}
-
-	return fmt.Errorf("getEvent not configured")
-}
-
-func testServer(ws *workflowServer) (net.Listener, error) {
-	l, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		return nil, fmt.Errorf("error starting listener: %s", err.Error())
-	}
-
-	go func() {
-		for {
-			conn, err := l.Accept()
-			if err != nil {
-				return
-			}
-
-			go func() {
-				wfsc := Workflow_ServerToClient(ws)
-
-				connection := rpc.NewConn(
-					rpc.StreamTransport(conn),
-					rpc.MainInterface(wfsc.Client),
-				)
-
-				// Wait for connection to abort.
-				<-connection.Done()
-			}()
-		}
-	}()
-
-	return l, nil
-}
 
 // TODO: remove backoff in case of test
 func TestConsume(t *testing.T) {
@@ -183,14 +89,6 @@ func TestConsumeAck(t *testing.T) {
 		t.Fatalf("Could not consume message: %s", err.Error())
 	}
 
-	if ref.AckID != ackID.String() {
-		t.Errorf("AckID isn't right. Got %s, want %s", ref.AckID, ackID.String())
-	}
-
-	if ref.EventID != eventID.String() {
-		t.Errorf("EventID isn't right. Got %s, want %s", ref.EventID, eventID.String())
-	}
-
 	if err := w.Ack(ref); err != nil {
 		t.Errorf("Ack failed: %s", err.Error())
 	}
@@ -248,74 +146,45 @@ func TestConsumeAckWithContent(t *testing.T) {
 		t.Fatalf("Could not consume message: %s", err.Error())
 	}
 
-	if ref.AckID != ackID.String() {
-		t.Errorf("AckID isn't right. Got %s, want %s", ref.AckID, ackID.String())
-	}
-
-	if ref.EventID != eventID.String() {
-		t.Errorf("EventID isn't right. Got %s, want %s", ref.EventID, eventID.String())
-	}
-
 	if err := w.Ack(ref, WithMessage(msg)); err != nil {
 		t.Errorf("Ack failed: %s", err.Error())
 	}
 }
 
-// TODO: update tests to use capnproto
-//
+func TestConsumeGet(t *testing.T) {
+	ackID := uuid.NewV4()
+	eventID := uuid.NewV4()
 
-/*
-// TODO: change this to use mux
-// and have mux.Handle() for paths
-func DefaultConsumeHandler(t *testing.T, w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/workers/workerid/work" {
-		w.WriteHeader(http.StatusOK)
+	srvr, err := testServer(&workflowServer{
+		getJob: func(getJob Workflow_getJob) error {
+			getJob.Results.SetAckID(ackID.Bytes())
+			getJob.Results.SetEventID(eventID.Bytes())
 
-		if err := json.NewEncoder(w).Encode(map[string]interface{}{
-			"ack_id":   "ackid",
-			"event_id": "eventid",
-			"flow_id":  "flowid",
-		}); err != nil {
-			t.Fatalf("Could not write work response: %v", err)
-		}
-	} else if r.URL.Path == "/flow/flowid/events/eventid" {
-		w.WriteHeader(http.StatusOK)
+			return nil
+		},
+		getEvent: func(getEvent Workflow_getEvent) error {
+			evt, err := getEvent.Results.NewEvent()
+			if err != nil {
+				return err
+			}
 
-		if err := json.NewEncoder(w).Encode(TestConsumeMessage); err != nil {
-			t.Fatalf("Could not write event response: %v", err)
-		}
-	} else if r.URL.Path == "/workers/workerid/ack/ackid" {
-		ar := ackRequest{}
-		if err := json.NewDecoder(r.Body).Decode(&ar); err != nil {
-			t.Fatalf("Could not parse ack request body: %v", err)
-		}
+			evt.SetContent(JsonContent("test"))
+			getEvent.Results.SetEvent(evt)
 
-		if diff := cmp.Diff(ackRequest{
-			Content: "content",
-			Metadata: map[string]interface{}{
-				"Test": "Test",
-			},
-			Filter: false,
-		}, ar); diff != "" {
-			t.Fatalf("Ack() request body mismatch (-want +got):\n%s", diff)
-		}
-	} else {
-		t.Fatalf("Unexpected path requested: %s", r.URL.Path)
+			return nil
+		},
+	})
+	if err != nil {
+		t.Fatalf("Could not start test server: %s", err.Error())
 	}
-}
 
-// TestConsumeAck will test the sequences of retrieving work, retrieving the event and ack'ing the event.
-func TestConsumeAck(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		DefaultConsumeHandler(t, w, r)
-	}))
-
-	defer ts.Close()
+	defer srvr.Close()
 
 	w, err := New(
-		MustWithRavenURL(ts.URL),
-		MustWithFlowID("flowid"),
-		MustWithWorkerID("workerid"),
+		MustWithRavenURL(fmt.Sprintf("capnproto://%s", srvr.Addr().String())),
+		MustWithFlowID(flowID.String()),
+		MustWithWorkerID(workerID.String()),
+		// TODO: WithMaxRetries(0),
 	)
 	if err != nil {
 		t.Fatalf("Could not initialize new raven worker: %s", err.Error())
@@ -328,141 +197,13 @@ func TestConsumeAck(t *testing.T) {
 
 	message, err := w.Get(ref)
 	if err != nil {
-		t.Fatalf("Could not consume message: %s", err.Error())
+		t.Errorf("Get failed: %s", err.Error())
 	}
 
-	want := TestConsumeMessage
-	got := map[string]interface{}{
-		"metadata": message.MetaData,
-		"content":  string(message.Content),
-	}
-
-	if diff := cmp.Diff(want, got); diff != "" {
-		t.Fatalf("Consume() mismatch (-want +got):\n%s", diff)
-	}
-
-	if err := w.Ack(ref, WithMessage(message)); err != nil {
-		t.Fatalf("Could not ack message: %s", err.Error())
+	if diff := cmp.Diff(message, Message{
+		MetaData: []Metadata{},
+		Content:  JsonContent("test"),
+	}); diff != "" {
+		t.Fatalf("Get() mismatch (-want +got):\n%s", diff)
 	}
 }
-
-// TestConsumeAck will test the sequences of retrieving work, retrieving the event and ack'ing the event.
-func TestConsumeAckWithUpdatedContent(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/workers/workerid/ack/ackid" {
-			ar := ackRequest{}
-			if err := json.NewDecoder(r.Body).Decode(&ar); err != nil {
-				t.Fatalf("Could not parse ack request body: %v", err)
-			}
-
-			if diff := cmp.Diff(ackRequest{
-				Content: "updated content",
-				Metadata: map[string]interface{}{
-					"Test": "Test",
-				},
-				Filter: false,
-			}, ar); diff != "" {
-				t.Fatalf("Ack() request body mismatch (-want +got):\n%s", diff)
-			}
-		} else {
-			DefaultConsumeHandler(t, w, r)
-		}
-	}))
-
-	defer ts.Close()
-
-	w, err := New(
-		MustWithRavenURL(ts.URL),
-		MustWithFlowID("flowid"),
-		MustWithWorkerID("workerid"),
-	)
-	if err != nil {
-		t.Fatalf("Could not initialize new raven worker: %s", err.Error())
-	}
-
-	ref, err := w.Consume()
-	if err != nil {
-		t.Fatalf("Could not consume message: %s", err.Error())
-	}
-
-	message, err := w.Get(ref)
-	if err != nil {
-		t.Fatalf("Could not consume message: %s", err.Error())
-	}
-
-	want := TestConsumeMessage
-	got := map[string]interface{}{
-		"metadata": message.MetaData,
-		"content":  string(message.Content),
-	}
-
-	if diff := cmp.Diff(want, got); diff != "" {
-		t.Fatalf("Consume() mismatch (-want +got):\n%s", diff)
-	}
-
-	message.Content = Content([]byte("updated content"))
-
-	if err := w.Ack(ref, WithMessage(message)); err != nil {
-		t.Fatalf("Could not ack message: %s", err.Error())
-	}
-}
-
-// TestConsumeAck will test the sequences of retrieving work, retrieving the event and ack'ing the event.
-func TestConsumeAckWithFilter(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/workers/workerid/ack/ackid" {
-			ar := ackRequest{}
-			if err := json.NewDecoder(r.Body).Decode(&ar); err != nil {
-				t.Fatalf("Could not parse ack request body: %v", err)
-			}
-
-			if diff := cmp.Diff(ackRequest{
-				Content: "content",
-				Metadata: map[string]interface{}{
-					"Test": "Test",
-				},
-				Filter: true,
-			}, ar); diff != "" {
-				t.Fatalf("Ack() request body mismatch (-want +got):\n%s", diff)
-			}
-		} else {
-			DefaultConsumeHandler(t, w, r)
-		}
-	}))
-
-	defer ts.Close()
-
-	w, err := New(
-		MustWithRavenURL(ts.URL),
-		MustWithFlowID("flowid"),
-		MustWithWorkerID("workerid"),
-	)
-	if err != nil {
-		t.Fatalf("Could not initialize new raven worker: %s", err.Error())
-	}
-
-	ref, err := w.Consume()
-	if err != nil {
-		t.Fatalf("Could not consume message: %s", err.Error())
-	}
-
-	message, err := w.Get(ref)
-	if err != nil {
-		t.Fatalf("Could not consume message: %s", err.Error())
-	}
-
-	want := TestConsumeMessage
-	got := map[string]interface{}{
-		"metadata": message.MetaData,
-		"content":  string(message.Content),
-	}
-
-	if diff := cmp.Diff(want, got); diff != "" {
-		t.Fatalf("Consume() mismatch (-want +got):\n%s", diff)
-	}
-
-	if err := w.Ack(ref, WithMessage(message), WithFilter()); err != nil {
-		t.Fatalf("Could not ack message: %s", err.Error())
-	}
-}
-*/
--- worker_test.go
+++ worker_test.go
@@ -1,5 +1,15 @@
 package ravenworker
 
+import (
+	"fmt"
+	"net"
+	"os"
+	"testing"
+
+	uuid "github.com/satori/go.uuid"
+	"zombiezen.com/go/capnproto2/rpc"
+)
+
 func MustWithRavenURL(s string) OptionFunc {
 	fn, err := WithRavenURL(s)
 	if err != nil {
@@ -22,4 +32,94 @@ func MustWithWorkerID(workerID string) OptionFunc {
 		panic(err)
 	}
 	return fn
+}
+
+var (
+	flowID   = uuid.NewV4()
+	workerID = uuid.NewV4()
+)
+
+// important to know is that you
+// cannot return t.Fatalf / t.FailNow()
+// from within go routines
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
+
+type workflowServer struct {
+	getEvent       func(getEvent Workflow_getEvent) error
+	getJob         func(getJob Workflow_getJob) error
+	ackJob         func(ackJob Workflow_ackJob) error
+	putEvent       func(putEvent Workflow_putEvent) error
+	getLatestEvent func(getLatestEvent Workflow_getLatestEvent) error
+}
+
+func (w *workflowServer) PutEvent(putEvent Workflow_putEvent) error {
+	if w.putEvent != nil {
+		return w.putEvent(putEvent)
+	}
+
+	return fmt.Errorf("putEvent not configured")
+}
+
+func (w *workflowServer) AckJob(ackJob Workflow_ackJob) error {
+	if w.ackJob != nil {
+		return w.ackJob(ackJob)
+	}
+
+	return fmt.Errorf("ackJob not configured")
+}
+
+func (w *workflowServer) GetJob(getJob Workflow_getJob) error {
+	if w.getJob != nil {
+		return w.getJob(getJob)
+	}
+
+	return fmt.Errorf("getJob not configured")
+}
+
+func (w *workflowServer) GetLatestEvent(getLatestEvent Workflow_getLatestEvent) error {
+	if w.getLatestEvent != nil {
+		return w.getLatestEvent(getLatestEvent)
+	}
+
+	return fmt.Errorf("getLatestEvent not configured")
+}
+
+func (w *workflowServer) GetEvent(getEvent Workflow_getEvent) error {
+	if w.getEvent != nil {
+		return w.getEvent(getEvent)
+	}
+
+	return fmt.Errorf("getEvent not configured")
+}
+
+func testServer(ws *workflowServer) (net.Listener, error) {
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return nil, fmt.Errorf("error starting listener: %s", err.Error())
+	}
+
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				return
+			}
+
+			go func() {
+				wfsc := Workflow_ServerToClient(ws)
+
+				connection := rpc.NewConn(
+					rpc.StreamTransport(conn),
+					rpc.MainInterface(wfsc.Client),
+				)
+
+				// Wait for connection to abort.
+				<-connection.Done()
+			}()
+		}
+	}()
+
+	return l, nil
 }
